c444e38e9bd2ba1d584fb61c2abea327740a8ea4
조가린 <41724184+gareen9342@users.noreply.github.com>
2021-06-11 15:17:15 +0900

    app에 있던 링크 header로 옮김

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Header from "./components/Header";
 import Feed from "./pages/Feed";
 import Home from "./pages/Home";
 
 import "./index.css";
+import StudyForm from "./pages/StudyForm";
+
 export default function App() {
   return (
     <Router>
-      <div class="w-64 h-32 mx-auto">
-        <ul className="flex items-stretch pl-16 p-4 rounded-full mx-auto bg-black text-yellow-600">
-          <li className="active:bg-green-700">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="px-12">
-            <Link to="/feed">feed</Link>
-          </li>
-        </ul>
-
+      <div>
+        <Header />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/feed" component={Feed} />
+          <Route path="/study/generate" component={StudyForm} />
           <Route path="*" render={() => <div>404</div>} />
         </Switch>
       </div>
---
+++ src/pages/StudyForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StudyForm = () => {
+  return <div className="container mx-auto">asdf</div>;
+};
+
+export default StudyForm;

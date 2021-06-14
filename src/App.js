848d6d5a24f094b07a424e4acefcb86bbcb54613
YSN <greeobedi91@gmail.com>
2021-06-14 13:37:57 +0900

    add map(load, resizing, current-location)

--- src/App.js
+++ src/App.js
@@ -5,24 +5,31 @@ import "../test.css";
 
 import Feed from "./pages/Feed";
 import Home from "./pages/Home";
-import Test from "./pages/Test";
+import KakaoMap from "./pages/Map/Map";
+
 import "./index.css";
+
 export default function App() {
+
   return (
     <Router>
-      <div class="w-64 h-32 mx-auto">
+      <div className="w-64 h-32 mx-auto">
         <ul className="flex items-stretch pl-16 p-4 rounded-full mx-auto bg-black text-yellow-600">
           <li className="active:bg-green-700">
             <Link to="/">Home</Link>
           </li>
-          <li className="px-12">
+          <li className="px-4">
             <Link to="/feed">feed</Link>
+          </li>
+          <li className="px-8 text-blue-400">
+            <Link to="/map">map</Link>
           </li>
         </ul>
 
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/feed" component={Feed} />
+          <Route exact path="/map" component={KakaoMap} />
           <Route path="*" render={() => <div>404</div>} />
         </Switch>
       </div>

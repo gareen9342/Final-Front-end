7dbf49bc9ad8ed25186c72911f11198c53c6211b
Changhee Park <arrans9591@gmail.com>
2021-06-14 12:52:05 +0900

    추가된 내용 : 네이버 로그인페이지 중요사항 : 이미지크기의 문제

--- src/App.js
+++ src/App.js
@@ -5,23 +5,28 @@ import Header from "./components/Header";
 import Feed from "./pages/Feed";
 import Home from "./pages/Home";
 import Kakao from "./pages/login/Kakao";
+import Naver from "./pages/login/Naver";
 
 import "./index.css";
 import StudyForm from "./pages/StudyForm";
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/feed" component={Feed} />
-          <Route path="/study/generate" component={StudyForm} />
-          <Route path="*" render={() => <div>404</div>} />
-        </Switch>
-      </div>
+    // <Router>
+    //   <div>
+    //     <Header />
+    //     <Switch>
+    //       <Route exact path="/" component={Home} />
+    //       <Route path="/feed" component={Feed} />
+    //       <Route path="/study/generate" component={StudyForm} />
+    //       <Route path="*" render={() => <div>404</div>} />
+    //     </Switch>
+    //   </div>
+    <>
+      <Naver/>
       <Kakao/>
-    </Router>
+    </>
+    
+    // </Router>
   );
 }

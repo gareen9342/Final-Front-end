98e0d6a21a4fe26fd5de6d5d51f3920694e77a86
조가린 <41724184+gareen9342@users.noreply.github.com>
2021-06-16 19:36:55 +0900

    주요사항 : 더미데이터 추가

--- src/pages/StudyForm/MapSearchForm.js
+++ src/pages/StudyForm/MapSearchForm.js
@@ -99,6 +99,7 @@ const MapSearchForm = ({
         // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
         setAddress(`${place.address_name} ${place.place_name}`);
         setAddressId(place.id);
+        console.log(`id = ${place.id}`);
         infoWindow.setContent(
           '<div style="padding:5px;font-size:12px;cursor:pointer;" >' +
             place.place_name +

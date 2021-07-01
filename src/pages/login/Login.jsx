0af2cbdac69dc6988cb039f08530ea6651c5aaaf
Changhee Park <arrans9591@gmail.com>
2021-07-01 14:11:00 +0900

    주요사항 : 로그인페이지 디자인 :star:

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -7,12 +7,27 @@ import Google from "./Google";
 const Login = ({signUserIn}) => {
 
     return(
-        <>  
-            <Kakao signUserIn={signUserIn}/> 
-            <Naver signUserIn={signUserIn}/>
-            <Google signUserIn={signUserIn}/>
-            <br/>
-            <button onClick={() => signUserIn("pass")} >다음화면으로 가려면 이거 눌러주세용</button>
+        <>
+        <div className="p-20 h-screen w-screen flex flex-col-reverse md:flex-row items-center justify-center bg-gray-200">
+            <div className="content text-3xl text-center md:text-left">
+                <h1 className="text-5xl text-blue-500 font-bold">SWith</h1>
+                <p>우리 같이 열정을 불태워봐요! 우리 같이 열정을 불태워봐요! 우리 같이 열정을 불태워봐요!</p>
+            </div>
+
+            <div className="container mx-auto flex flex-col items-center">
+                <form className="shadow-lg w-80 p-4 flex flex-col bg-white rounded-lg">
+                    <Kakao signUserIn={signUserIn}/> 
+                    <br/>
+                    <Naver signUserIn={signUserIn}/>
+                    <br/>
+                    <Google signUserIn={signUserIn}/>
+                    <br/>
+                    <hr />
+                    <button onClick={() => signUserIn("pass")} className="w-full bg-green-400 mt-8 mb-4 text-white p-3 rounded-lg font-semibold text-lg">다음으로 가려면 <br/> 여기 눌러주세용</button>
+                </form>
+            </div>
+        </div>
+            
         </>
     )
 }

5252423dd83572d25a333b4a064701395482bb4a
AppleTrick <arrans9591@gmail.com>
2021-07-02 12:16:12 +0900

    주요사항 : localstorage 구현 :star:

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import StudyForm from "./pages/StudyForm";
 import MyStudy from "./pages/MyStudy";
 
 import { signIn } from "./pages/login/Auth";
+// import { useLocalStorage } from "./services/useLocalStorage";
 
 export default function App() {
   
@@ -34,11 +35,7 @@ export default function App() {
   const logout = () => {
     setUserEmail(null);
     setYesUser(null);
-  }
-  
-  const saveData = () => {
-    const userObj = {email : userEmail}
-    window.localStorage.setItem("email",JSON.stringify(userObj));
+    window.localStorage.removeItem("email");
   }
   
 
@@ -55,10 +52,9 @@ export default function App() {
     }else{
       setUserEmail(email);
       setYesUser(true);
-      saveData();
+      window.localStorage.setItem("email",email);
 
-      console.log("로컬스토레이지 작동",window.localStorage.getItem("email"));
-
+      console.log("storage value : ", window.localStorage.getItem("email"));
       console.log("로그인한 회원임");
     }
     

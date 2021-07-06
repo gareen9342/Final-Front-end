148b32c910c766914d3784461582c2239aff513b
GIwon Lim <l4279625@gmail.com>
2021-07-07 03:01:54 +0900

    주요사항 : 결제기능 완료, Spring단 처리 해줘야함 :star:

--- src/pages/Iamport/UI/PaymentCustomRadio.js
+++ src/pages/Iamport/UI/PaymentCustomRadio.js
@@ -10,14 +10,14 @@ export const PaymentCustomRadio = ({id, title, content, onchange}) => {
             </div>
             <div className="">
                 <select
-                    className="h-3 p-6 w-full border-2 border-gray-300 mb-5 rounded-md z-0"
+                    className="h-15 p-3 w-full border-2 border-gray-300 mb-3 rounded-md z-0"
                     id={id}
                     value={content}
                     onChange={onchange}
                 >
                     <option value="신용카드">신용카드</option>
                     <option value="계좌이체">계좌이체</option>
-                    <option value="가상계좌">가상계좌</option>
+                    <option value="무통장입금">무통장입금</option>
                     <option value="카카오페이">카카오페이</option>
                     <option value="페이코">페이코</option>
                 </select>

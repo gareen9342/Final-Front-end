8e1ca2056bbbe5e44f4f8efc012a50c81a2e87a1
조가린 <41724184+gareen9342@users.noreply.github.com>
2021-07-13 13:11:17 +0900

    주요사항 :피드수정 :star:

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -9,11 +9,12 @@ export default () => {
       (async () => {
         try {
           setLoading(true);
-          const { data } = FeedService.selectFeeds(
+          const { data } = await FeedService.selectFeeds(
             localStorage.getItem("email")
           );
+
           if (!!data) {
-            setFeeds(data);
+            setFeeds(data.list);
           }
         } catch (err) {
           console.error(err);
@@ -29,18 +30,22 @@ export default () => {
   }, []);
   return (
     <>
-      {/* <Container>
-        {console.log("feeds", feeds)}
-        <Card content={"피드 내용"} username={"가린"} />
-      </Container> */}
-      <Bar username="username" />
+      {" "}
+      {console.log("feeds", feeds)}
+      <Bar username={localStorage.getItem("email")} />
       <div class="bg-gray-50 min-h-screen flex items-center justify-center px-16">
         <div class="relative w-full max-w-lg">
           <div class="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob "></div>
           <div class="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
           <div class="absolute -bottom-32 left-20 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
           <div class="m-8 relative space-y-4">
-            <Card content={"content"} username={"username"} />
+            {feeds.map((feed) => (
+              <Card
+                key={feed.feedid}
+                content={feed.feedcontent}
+                // username={feed.memberemail.substring(0, 8)}
+              />
+            ))}
           </div>
         </div>
       </div>
